7dea3be99d8b89c0186f5f17ec52b455223501f1
Erik Gomez <e@eriknicolasgomez.com>
2025-03-03 20:25:17 -0600

    bring back missing config

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+
+export default {
+    title: "Meticulous Profiles",
+    description: "A site powered by VitePress",
+    base: "/met-profiles/"
+}
